41faa260e5a52f910465f0ed67914896257d84e6
stanislav430 <danyliukstas1@gmail.com>
2024-06-13 08:40:54 -0400

    Add Hero section

---
+++ src/db/main.ts
@@ -0,0 +1,11 @@
+import profileImage from "../../public/images/stanislav.png";
+
+export const heroData = {
+  img: profileImage,
+  hi: "Hello! I am",
+  name: "Stanislav Danyliuk",
+  expertise: ["Front-End Developer", "Mobile App Developer"],
+  about:
+    "I'm a passionate Front-End Developer based in Ukraine, currently studying Software Engineering with a strong interest in UI/UX design. I'm committed to creating visually stunning and user-friendly websites, and I'm looking forward to exploring new opportunities to enhance my skills.",
+  btnText: "Get my resume",
+};
